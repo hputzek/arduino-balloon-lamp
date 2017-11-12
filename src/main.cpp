ee45e91e0941d3950816e3a918a3004d09eb7fa5
Hendrik Putzek <hendrik@putzek.com>
2017-11-12 19:37:39 +0100

    Initial commit: setup project structure

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+/*
+	A LED Contnroller using Arduino for WS2811 LED's.
+  Used for a balloon shaped lamp I will build with my nephew.
+
+  Refer to github repo for more information.
+
+  Created 11/2017 by Hendrik Putzek
+
+*/
+
+
+// https://github.com/mathertel/OneButton/
+#include "OneButton.h";
+// https://github.com/FastLED/FastLED
+#include "FastLED.h";
+// https://github.com/rocketscream/Low-Power/
+# include "LowPower.h";
+
+void setup() {
+
+}
+
+void loop() {
+
+}
